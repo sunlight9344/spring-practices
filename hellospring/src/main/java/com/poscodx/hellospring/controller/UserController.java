3df6d7f664310e2096aa815bb15ae4e931ccb8ef
sunlight9344 <sunlight9344@gmail.com>
2023-09-15 10:40:55 +0900

    @RequestParam
    @RequestMapping
    
    @RequestPathMapping

--- hellospring/src/main/java/com/poscodx/hellospring/controller/UserController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/UserController.java
@@ -2,6 +2,9 @@ package com.poscodx.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @RequestMapping 클래스 + 메서드 매핑
@@ -13,10 +16,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 	
-	@RequestMapping("/joinform")
+	@RequestMapping(value="/join", method=RequestMethod.GET)
 	public String joinform() {
-		return "/WEB-INF/views/joinform.jsp";
+		return "/WEB-INF/views/join.jsp";
 	}
 	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		System.out.println("UserController.join(): UserDao.insert(" + vo + ")");
+		return "redirect:/";
+	}
 	
+	@ResponseBody
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {
+		/**
+		 * 만일 n이라는 이름의 URL 파라미터가 없으면
+		 * 400 bad request 에러가 난다요.
+		 */
+		return "UserController.update(" + name + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value="n", required=false, defaultValue="") String name) {
+		return "UserController.update(" + name + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list(@RequestParam(value="p", required=false, defaultValue="1") String pageNo) {
+		return "UserController.list(" + pageNo + ")";
+	}
 }
