930caca732379521f9616bee8b4fd817a4f0e1e2
sunlight9344 <sunlight9344@gmail.com>
2023-09-21 14:59:45 +0900

    fileupload #2

---
+++ fileupload/src/main/java/com/poscodx/fileupload/service/FileUploadService.java
@@ -0,0 +1,30 @@
+package com.poscodx.fileupload.service;
+
+import java.io.File;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileUploadService {
+	
+	private static String SAVE_PATH = "/mysite-uploads";
+	
+	public String restore(MultipartFile file) {
+		String url = null;
+		
+		File uploadDirectory = new File(SAVE_PATH);
+		if(!uploadDirectory.exists()) {
+			uploadDirectory.mkdir();
+		}
+		
+		if(file.isEmpty()) {
+			return url;
+		}
+		
+		System.out.println(file.getOriginalFilename());
+		
+		return url;
+	}
+
+}
