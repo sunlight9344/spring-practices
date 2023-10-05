ddf284c693b1910e6e810d01cd4beb9fd088d892
sunlight9344 <sunlight9344@gmail.com>
2023-10-05 10:50:38 +0900

    container #3

---
+++ container/src/main/java/com/poscodx/container/videosystem/BlankDisc.java
@@ -0,0 +1,42 @@
+package com.poscodx.container.videosystem;
+
+import java.util.List;
+
+public class BlankDisc implements DigitalVideoDisc {
+	
+	private String title;
+	private String studio;
+	private List<String> actors;
+	
+	public BlankDisc(String title, String studio) {
+		this.title = title;
+		this.studio = studio;
+	}
+	
+	public BlankDisc(String title, String studio, List<String> actors) {
+		this.title = title;
+		this.studio = studio;
+		this.actors = actors;
+	}
+	
+	@Override
+	public String play() {
+		return "Playing Movie " + studio + "'s " + title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStudio() {
+		return studio;
+	}
+
+	public void setStudio(String studio) {
+		this.studio = studio;
+	}
+}
